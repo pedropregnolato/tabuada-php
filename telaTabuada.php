d78037a9068a14043d594473178273649e929735
Pedro Lucas Nunes Pregnolato <pregnolato.pedro@NEVESCOSTA.local>
2022-02-18 10:36:39 -0300

    front corrigido

--- telaTabuada.php
+++ telaTabuada.php
@@ -4,23 +4,23 @@ $j = 1;
 $n = 1;
 while ($n <= 10) {
     $i = 1;
+?>
+    <h3> Tabuada do <?php echo $n; ?> </h3>
+    <?php
     while ($i <= 10) {
         $n2 = $n * $i;
-?>
-
+    ?>
         <table>
             <td>
                 <tr>
                     <?php echo $n . " x " . $i . " = " . $n2; ?>
                 </tr>
-
-            <?php
-            $i++;
-        }
-            ?>
-                <h3> tabuada do <?php echo $n; ?> </h3><br>
             </td>
-        </table>
+        <?php
+        $i++;
+    }
+        ?>
+        </table><br><br>
     <?php
     $n++;
 }
